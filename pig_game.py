983b5469d1d52b54cc228c1a793dde427a76670c
felix <61442272+felix-prabowo@users.noreply.github.com>
2024-05-22 12:03:24 +0700

    Establish the game mechanism with a max score of 50

--- pig_game.py
+++ pig_game.py
@@ -22,3 +22,28 @@ while True:
 
 max_score = 50
 player_scores = [0 for _ in range(players)]    # a list comprehension   
+
+while max(player_scores) < max_score:
+    
+    for player_index in range(players):
+        print(f"\nPlayer number {player_index + 1} turn has just started!\n")
+        current_score = 0
+        
+        while True:
+            should_roll = input("Would you like to roll (y)? ")
+            if should_roll.lower() != "y":
+                break
+
+            value = roll()
+            if value == 1:
+                print("You rolled a 1! Turn done!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("You rolled a:", value)
+
+            print("Your score is:", current_score)
+    
+        player_scores[player_index] += current_score
+        print("Your total score is:", player_scores[player_index])
